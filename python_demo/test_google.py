9b39db7c0849f06517ef9447ea486f5ae58b9427
Tomer <tomer.bu@gmail.com>
2024-03-20 16:50:51 +0200

    firefox

--- python_demo/test_google.py
+++ python_demo/test_google.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
-
+from webdriver_manager.core.os_manager import ChromeType
 
 def chrome():
   driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
@@ -14,12 +14,18 @@ def firefox():
   driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
   return driver 
 
+def chromium():
+  driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+  return driver
+
+
+
 
 import time
 
 
 def test_title():
-  driver = firefox()
+  driver = chromium()
   driver.get("http://www.google.com")
   assert driver.title == "Google"
   time.sleep(5)
