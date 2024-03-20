c407ca7f715b239d17e3d7aeee5f3053447e93d5
Tomer <tomer.bu@gmail.com>
2024-03-20 16:33:23 +0200

    update to webdriver

--- python_demo/test_google.py
+++ python_demo/test_google.py
@@ -1,5 +1,13 @@
+# selenium 4
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+
 import time
+
 
 def test_title():
   driver = webdriver.Chrome()
@@ -7,3 +15,7 @@ def test_title():
   assert driver.title == "Google"
   time.sleep(5)
   driver.quit()
+
+# remove python_demo/__pycache__/test_google.cpython-311-pytest-8.1.1.pyc from git:
+# remove the entire folder python_demo/__pycache__ from git:
+# git rm -r python_demo/__pycache__
