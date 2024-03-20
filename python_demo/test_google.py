da92e8523bd286212874a056d83aeacc05adc0e3
Tomer <tomer.bu@gmail.com>
2024-03-20 16:40:16 +0200

    bugfix

--- python_demo/test_google.py
+++ python_demo/test_google.py
@@ -1,16 +1,17 @@
-# selenium 4
+  # selenium 4
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-
+def chrome():
+  driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+  return driver
 
 import time
 
 
 def test_title():
-  driver = webdriver.Chrome()
+  driver = chrome()
   driver.get("http://www.google.com")
   assert driver.title == "Google"
   time.sleep(5)
