eed9042fe448fca589ed55c714dc1247edc88830
Tomer <tomer.bu@gmail.com>
2024-03-25 14:39:26 +0200

    chrome

--- python_demo/test_google.py
+++ python_demo/test_google.py
@@ -8,18 +8,22 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
+def chrome():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    # options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(service=Service(
+        ChromeDriverManager().install()), options=options)
+    return driver
+
+
 def test_title():
-  options = Options()
-  options.add_argument('--headless')
-  options.add_argument('--no-sandbox')
-  # options.add_argument('--disable-dev-shm-usage')
-  driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver = chrome()
 
-  driver.get("https://python.org")
-  assert "Python" in driver.title
-  driver.close()
-
-
+    driver.get("https://python.org")
+    assert "Python" in driver.title
+    driver.close()
 
 
 # docker ps
