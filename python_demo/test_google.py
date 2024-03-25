f2f2f58e3f3e335c9c0cdeb83980ad45f90dc394
Tomer <tomer.bu@gmail.com>
2024-03-25 14:02:11 +0200

    chrome

--- python_demo/test_google.py
+++ python_demo/test_google.py
@@ -1,11 +1,16 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 def test_title():
-  # headless mode
-  options = webdriver.ChromeOptions()
+  options = Options()
+  options.binary_location = "/usr/bin/chromium"
   options.add_argument("--headless")
-  driver = webdriver.Chrome(options=options)
+
+  s = Service("/usr/local/bin/chromedriver")
+
+  driver = webdriver.Chrome(service=s, options=options)
   driver.get("http://www.google.com")
   assert driver.title == "Google"
   time.sleep(5)
